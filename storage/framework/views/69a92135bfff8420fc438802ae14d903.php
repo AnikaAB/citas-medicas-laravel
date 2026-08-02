6f923b1b4628e009671af1b6aad81c0ae0d08bea
Ana Vasquez <anavasquezmisho@gmail.com>
2026-08-02 13:15:53 -0500

    Corrige gitignore para excluir cache, sesiones y archivos autogenerados

--- storage/framework/views/69a92135bfff8420fc438802ae14d903.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php $__env->startSection('titulo', 'Sesión expirada'); ?>
-<?php $__env->startSection('codigo', '419'); ?>
-<?php $__env->startSection('mensaje', 'Tu sesión ha expirado por seguridad. Por favor, inicia sesión nuevamente para continuar.'); ?>
-
-<?php echo $__env->make('errors.layout', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\EDUARDO VIDAL\Desktop\citas-medicas-laravel\resources\views/errors/419.blade.php ENDPATH**/ ?>
